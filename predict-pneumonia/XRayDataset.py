27a40d4382695e07c1d7fbdb0e8b26c0f1894b0f
Josemaria Macedo Carrillo <josemaria@linux5.cs.uchicago.edu>
2023-04-21 15:29:01 -0500

    Erase transformations from XRayDataset class

--- predict-pneumonia/XRayDataset.py
+++ predict-pneumonia/XRayDataset.py
@@ -4,24 +4,8 @@ from torch.utils import data
 from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as T
 from torchvision.io import read_image
-
 import cv2
 
-resize = T.Compose([
-            T.ToPILImage(), 
-            T.Resize((int(968.07), int(968.07/0.7146))), # Resize the image to match mean ratio
-            T.ToTensor(), 
-            T.Normalize(mean=[0.7146]*3, std=[0.1185]*3) # Normalize the image
-        ])
- 
-transforms = T.Compose([
-            T.ToPILImage(), 
-            T.RandomAdjustSharpness(sharpness_factor=0.5),
-            T.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5)),
-            T.Resize((int(968.07), int(968.07/0.7146))), # Resize the image to match mean ratio
-            T.ToTensor(), 
-            T.Normalize(mean=[0.7146]*3, std=[0.1185]*3)
-        ])
 
 class XRayDataset(Dataset):
     def __init__(self, csv_file, transform=None):
