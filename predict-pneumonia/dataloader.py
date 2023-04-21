b4b83ad5db418031a766b2587af1ad7cc2202aad
Lisette Solis <lisettesolis@linux7.cs.uchicago.edu>
2023-04-21 15:44:36 -0500

    change to transform

--- predict-pneumonia/dataloader.py
+++ predict-pneumonia/dataloader.py
@@ -5,17 +5,15 @@ import torchvision.transforms as T
 resize = T.Compose([
             T.ToPILImage(), 
             T.Resize((int(968.07), int(968.07/0.7146))), # Resize the image to match mean ratio
-            T.ToTensor(), 
-            T.Normalize(mean=[0.7146]*3, std=[0.1185]*3) # Normalize the image
+            T.ToTensor()
         ])
  
 transforms = T.Compose([
             T.ToPILImage(), 
-            T.RandomAdjustSharpness(sharpness_factor=0.5),
-            T.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5)),
+            T.RandomAdjustSharpness(sharpness_factor=10),
+            T.ColorJitter(brightness=.5, hue=.3),
             T.Resize((int(968.07), int(968.07/0.7146))), # Resize the image to match mean ratio
-            T.ToTensor(), 
-            T.Normalize(mean=[0.7146]*3, std=[0.1185]*3)
+            T.ToTensor()
         ])
 
 training_data = XRayDataset("/home/josemaria/30254-pneumoniacs/predict-pneumonia/data/csv_files/train.csv", transforms)
